d68b0194df06535a6d58455ec3ee11f2bc7c6362
Luca Scotzniovsky <lucascotzniovsky@Lucas-MBP.attlocal.net>
2020-06-25 00:01:04 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='supersonic_wind_tunnel',
+    version='1',
+    packages=[
+        'supersonic_wind_tunnel',
+    ],
+    install_requires=[
+    ],
+)
